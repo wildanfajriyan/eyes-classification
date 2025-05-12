999910ea8b7386d9c1ad74316fc125d824c2f61f
wildanuwil <wildanfajriyann@gmail.com>
2025-05-12 16:55:46 +0700

    adjust layout

--- App.tsx
+++ App.tsx
@@ -1,22 +1,35 @@
-import React, { useEffect, useRef, useState } from "react";
-import { View, StyleSheet, TouchableOpacity, Image, Text, Button, Pressable } from "react-native";
-import * as tf from "@tensorflow/tfjs";
-import { bundleResourceIO, decodeJpeg } from "@tensorflow/tfjs-react-native";
-import * as ImagePicker from "expo-image-picker";
-import * as FileSystem from "expo-file-system";
-import * as jpeg from "jpeg-js";
-import * as mobilenet from "@tensorflow-models/mobilenet";
-import {CameraView, CameraType, useCameraPermissions, Camera, CameraMode } from "expo-camera";
-
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Text,
+  Button,
+  Pressable,
+} from 'react-native';
+import * as tf from '@tensorflow/tfjs';
+import { bundleResourceIO, decodeJpeg } from '@tensorflow/tfjs-react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+import * as jpeg from 'jpeg-js';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import {
+  CameraView,
+  CameraType,
+  useCameraPermissions,
+  Camera,
+  CameraMode,
+} from 'expo-camera';
 
 export default function App() {
   const [isTfReady, setIsTfReady] = useState(false);
-  const [result, setResult] = useState("");
-  const [pickedImage, setPickedImage] = useState("");
+  const [result, setResult] = useState('');
+  const [pickedImage, setPickedImage] = useState('');
   const [permission, requestPermission] = useCameraPermissions();
   const ref = useRef<CameraView>(null);
-  const [mode, setMode] = useState<CameraMode>("picture");
-  const [facing, setFacing] = useState<CameraType>("back");
+  const [mode, setMode] = useState<CameraMode>('picture');
+  const [facing, setFacing] = useState<CameraType>('back');
   const [showCamera, setShowCamera] = useState(false);
 
   useEffect(() => {
@@ -24,9 +37,9 @@ export default function App() {
   }, [pickedImage]);
 
   const getTensor = async () => {
-    const modelWeightOne = await require("./group1-shard1of2.bin");
-    const modelWeightTwo = await require("./group1-shard2of2.bin");
-    const modelJson = await require("./model.json");
+    const modelWeightOne = await require('./group1-shard1of2.bin');
+    const modelWeightTwo = await require('./group1-shard2of2.bin');
+    const modelJson = await require('./model.json');
     const model = await tf.loadLayersModel(
       bundleResourceIO(modelJson, [modelWeightOne, modelWeightTwo])
     );
@@ -37,7 +50,7 @@ export default function App() {
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ["images"],
+      mediaTypes: ['images'],
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
@@ -59,19 +72,19 @@ export default function App() {
 
   const classify = async () => {
     try {
-      setResult("");
+      setResult('');
       // Load mobilenet
       await tf.ready();
       // const model = await mobilenet.load();
       const model = await getTensor();
       setIsTfReady(true);
-      console.log("starting inference with picked image: " + pickedImage);
+      console.log('starting inference with picked image: ' + pickedImage);
 
       // Convert image to tensor
       const imgB64 = await FileSystem.readAsStringAsync(pickedImage, {
         encoding: FileSystem.EncodingType.Base64,
       });
-      const imgBuffer = tf.util.encodeString(imgB64, "base64").buffer;
+      const imgBuffer = tf.util.encodeString(imgB64, 'base64').buffer;
       const raw = new Uint8Array(imgBuffer);
 
       const imageTensor = decodeJpeg(raw);
@@ -89,12 +102,10 @@ export default function App() {
       const prediction = await makePredictions(model, img);
 
       if (prediction) {
-        setResult(
-          prediction.toFixed(4)
-        );
+        setResult(prediction.toFixed(4));
       }
 
-      console.log("pred", JSON.stringify(prediction));
+      console.log('pred', JSON.stringify(prediction));
       // return prediction.toString();
     } catch (err) {
       console.log(err);
@@ -102,7 +113,7 @@ export default function App() {
   };
 
   function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
+    setFacing((current) => (current === 'back' ? 'front' : 'back'));
   }
 
   if (!permission) {
@@ -112,7 +123,7 @@ export default function App() {
   if (!permission.granted) {
     return (
       <View style={styles.container}>
-        <Text style={{ textAlign: "center" }}>
+        <Text style={{ textAlign: 'center' }}>
           We need your permission to use the camera
         </Text>
         <Button onPress={requestPermission} title="Grant permission" />
@@ -122,114 +133,106 @@ export default function App() {
 
   const takePicture = async () => {
     const photo = await ref.current?.takePictureAsync();
-    setPickedImage(photo?.uri)
+    setPickedImage(photo?.uri);
     setShowCamera(false);
     // setUri(photo?.uri);
   };
 
-  const toggleMode = () => {
-    setMode((prev) => (prev === "picture" ? "video" : "picture"));
-  };
-
   const toggleFacing = () => {
-    setFacing((prev) => (prev === "back" ? "front" : "back"));
+    setFacing((prev) => (prev === 'back' ? 'front' : 'back'));
   };
 
   const closeCamera = () => {
     setShowCamera(false);
-    // Optionally pause preview
+
     if (ref.current) {
       ref.current?.pausePreview(); // or cameraRef.current.stopRecording();
     }
   };
 
+  if (showCamera) {
+    return (
+      <CameraView
+        style={styles.camera}
+        ref={ref}
+        mode={mode}
+        facing={facing}
+        mute={false}
+        ratio='4:3'
+        responsiveOrientationWhenOrientationLocked
+      >
+        <View style={styles.shutterContainer}>
+        {/* <Button title="Close Camera" onPress={closeCamera} /> */}
+        <Pressable onPress={closeCamera}>
+            {/* <FontAwesome6 name="rotate-left" size={32} color="white" /> */}
+            <Text style={{color: 'white'}}>close</Text>
+          </Pressable>
+
+          <Pressable onPress={takePicture}>
+            {({ pressed }) => (
+              <View
+                style={[
+                  styles.shutterBtn,
+                  {
+                    opacity: pressed ? 0.5 : 1,
+                  },
+                ]}
+              >
+                <View
+                  style={[
+                    styles.shutterBtnInner,
+                    {
+                      backgroundColor: mode === 'picture' ? 'white' : 'red',
+                    },
+                  ]}
+                />
+              </View>
+            )}
+          </Pressable>
+          <Pressable onPress={toggleFacing}>
+            {/* <FontAwesome6 name="rotate-left" size={32} color="white" /> */}
+            <Text style={{color: 'white'}}>rotate</Text>
+          </Pressable>
+        </View>
+      </CameraView>
+    );
+  }
+
   return (
     <View
       style={{
         height: '100%',
-        backgroundColor: 'red',
         display: 'flex',
         flexDirection: 'column',
-        // alignItems: 'center',
-        // justifyContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
-
-      {showCamera && (
- <CameraView
- style={styles.camera}
- ref={ref}
- mode={mode}
- facing={facing}
- mute={false}
- responsiveOrientationWhenOrientationLocked
->
- <View style={styles.shutterContainer}>
- <Button title="Close Camera" onPress={closeCamera} />
-
-   <Pressable>
-     {mode === "picture" ? (
-       // <AntDesign name="picture" size={32} color="white" />
-       <Text>Picture</Text>
-     ) : (
-       // <Feather name="video" size={32} color="white" />
-       <></>
-     )}
-   </Pressable>
-   <Pressable onPress={takePicture}>
-     {({ pressed }) => (
-       <View
-         style={[
-           styles.shutterBtn,
-           {
-             opacity: pressed ? 0.5 : 1,
-           },
-         ]}
-       >
-         <View
-           style={[
-             styles.shutterBtnInner,
-             {
-               backgroundColor: mode === "picture" ? "white" : "red",
-             },
-           ]}
-         />
-       </View>
-     )}
-   </Pressable>
-   <Pressable onPress={toggleFacing}>
-     {/* <FontAwesome6 name="rotate-left" size={32} color="white" /> */}
-     <Text>rotate</Text>
-   </Pressable>
- </View>
-
-</CameraView>
-      )}
-     
-
       <Image
         source={{ uri: pickedImage }}
         style={{ width: 200, height: 200, margin: 40 }}
       />
 
-      {/* <Text>{uri}</Text> */}
+      <View style={{ display: 'flex', alignItems: 'center', gap: 20 }}>
+        <View style={{ display: 'flex', flexDirection: 'row', gap: 10 }}>
+          {isTfReady && <Button onPress={pickImage} title="Upload Image" />}
+          <Button title="Open Camera" onPress={() => setShowCamera(true)} />
+        </View>
 
-      {isTfReady && <Button onPress={pickImage} title="Upload Image" />}
-      <Button title="Open Camera" onPress={() => setShowCamera(true)} />
+        {!isTfReady && <Text>LOADING...</Text>}
 
-      {!isTfReady && <Text>LOADING...</Text>}
-
-      {result !== '' || !pickedImage ? (
-        <Text>
-          {Number(result) === 0
-            ? ''
-            : Number(result) > 0.5
-            ? 'Normal'
-            : 'Cataract'}
-        </Text>
-      ) : (
-        <Text>LOADING...</Text>
-      )}
+        {result !== '' || !pickedImage ? (
+          <Text style={{ fontWeight: 'bold' }}>
+            {Number(result) === 0
+              ? ''
+              : Number(result) > 0.5
+              ? 'Normal'
+              : 'Cataract'}
+          </Text>
+        ) : (
+          <Text>LOADING...</Text>
+        )}
+      </View>
     </View>
   );
 }
@@ -237,33 +240,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#fff'
   },
   camera: {
     flex: 1,
-    width: "100%",
+    width: '100%',
   },
   shutterContainer: {
-    position: "absolute",
-    bottom: 44,
+    position: 'absolute',
+    bottom: 70,
     left: 0,
-    width: "100%",
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "space-between",
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     paddingHorizontal: 30,
   },
   shutterBtn: {
-    backgroundColor: "transparent",
+    backgroundColor: 'transparent',
     borderWidth: 5,
-    borderColor: "white",
+    borderColor: 'white',
     width: 85,
     height: 85,
     borderRadius: 45,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
   },
   shutterBtnInner: {
     width: 70,
